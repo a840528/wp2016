b391f3ad66755a4391ba7d6eea0250478b9865d3
a840528 <a840528@gmail.com>
2016-10-19 13:56:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-
-$(".button").click(
+$(".adobe").click(
   function(){
-    $(this).addClass("afterbutton")
+    $(this).addClass("afteradobe")
+  });
+$(".afteradobe").click(
+  function(){
+    $(this).rmClass("afteradobe")
+  });
+$(".ai").click(
+  function(){
+    $(this).text("AI ▐▐▐▐▐▐▐▐")
+  });
+$(".ps").click(
+  function(){
+    $(this).text("PS ▐▐▐▐▐▐")
+  });
+$(".ae").click(
+  function(){
+    $(this).text("AE ▐")
+  });
+$(".pr").click(
+  function(){
+    $(this).text("PR ▐▐▐")
   });
